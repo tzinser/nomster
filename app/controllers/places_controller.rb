523e8e6eda930bea448dee593f5b1d2680c3639b
Tina Zinser <zinsert7@gmail.com>
2015-07-16 13:21:41 +0000

    Pagination changed to 5 per page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
 	def index
-		@places = Place.all.paginate(:page => params[:page], :per_page => 1)
+		@places = Place.all.paginate(:page => params[:page], :per_page => 5)
 	end
 
 	def new
