4cab0382ebb8979ca6b3881c744db0a9fcf55ee9
Tina Zinser <zinsert7@gmail.com>
2015-07-23 19:23:53 +0000

    Lesson 43, auto trigger email notification of new comment added.  Disabled code for email delivery

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,17 +4,20 @@ Nomster::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.action_mailer.delivery_method = :smtp
+  
+  # Code used for triggering email delivery
+  # config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD']
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: "example.com",
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: ENV['GMAIL_ADDRESS'],
+  #   password: ENV['GMAIL_PASSWORD']
+  # }
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
